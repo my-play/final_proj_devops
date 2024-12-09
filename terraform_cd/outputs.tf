8261483ef06b7a02e17fb9ce0e3eedb2f37f615d
timuraa <timur.abdulin@transmitsecurity.com>
2024-12-09 18:11:08 +0200

    add ec2 ip output

--- terraform_cd/outputs.tf
+++ terraform_cd/outputs.tf
@@ -6,6 +6,11 @@ output "subnet_id" {
   value = aws_subnet.main.id
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.k8s_node.public_ip
+  description = "Public IP address of the EC2 instance"
+}
+
 output "ec2_instance_id" {
   value = aws_instance.k8s_node.id
 }
